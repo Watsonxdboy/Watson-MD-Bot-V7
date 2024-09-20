56f28661c6acc3032343e0dfa5b7bfea24d8d4ef
WATSON-XD2 <fourpencewatson41@gmail.com>
2024-09-20 20:39:57 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "", //Add Your Session id
+ALIVE_IMG : process.env.ALIVE_IMG || "https://telegra.ph/file/b7ae2db82899be94fb9c0.jpg",
+ALIVE_MSG : process.env.ALIVE_MSG || "Put your alive messege",
+MENU_IMG : process.env.MENU_IMG || "https://telegra.ph/file/b7ae2db82899be94fb9c0.jpg",
+MODE : process.env.MODE || "public",    
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+AUTO_BIO: process.env.AUTO_BIO || "true",
+READ_CMD: process.env.READ_CMD || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
+OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
+};
